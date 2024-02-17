3c810b15257efc7e41e68faa8b32bd07015ede26
javleo30 <javierjleons@gmail.com>
2024-02-16 19:44:01 -0500

    actualizacion y implementacion de app store

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -37,8 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.humanize',
     "category",
     "accounts",
+    "store",
     
 
 ]
@@ -126,6 +128,13 @@ STATIC_ROOT = BASE_DIR /'static'
 STATICFILES_DIRS = [
     "ecommerce/static"
 ]
+
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / 'media'
+
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
